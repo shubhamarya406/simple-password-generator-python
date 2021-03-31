23b88a4c45d575f3599a44f0e4e3e672395269b2
Shubham Arya <shubhamarya406@gmail.com>
2021-03-31 08:11:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Password Generator Project
+import random
+from art import logo
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print(logo+"\n")
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+lc=sc=nc=0
+passwd=[]
+for i in range(0,nr_letters):
+  passwd+=random.choice(letters)
+for i in range(0,nr_symbols):
+  passwd+=random.choice(symbols)
+for i in range(0,nr_numbers):
+  passwd+=random.choice(numbers)
+
+password=''.join(passwd)
+print(f"\nThe generated password is {password}")
